dc85f544dfdfebd08a87a9a7bf631a93ea3f516e
draganglumac <dragan.glumac@gmail.com>
2013-12-10 10:42:22 +0000

    Added extra routes for handling dynamic proxy configuration.

--- server.rb
+++ server.rb
@@ -5,14 +5,28 @@ require 'helpers'
 
 include Helpers
 
-not_found do
-  "Your device IP didn't match any node so I don't know where to redirect your request. Please check your configuration."
+set :proxy_routes, {}
+
+post '/SKG/proxy/add_route' do
+  add_route_for_source_ip_of_request
+end
+
+post '/SKG/proxy/remove_route' do
+  remove_route_for_source_ip_of_request
+end
+
+get '/SKG/proxy/remove_route' do
+  remove_route_for_source_ip_of_request
 end
 
 get '/SKG/config.htm' do
   device_ip = request.env['REMOTE_ADDR']
   node_ip = find_node_ip_from_connected_device_ip(device_ip)
-  puts "node_ip = #{node_ip} for device_ip = #{device_ip}"
 
-  redirect "http://#{node_ip}/SKG/config.htm" if not node_ip.nil?
+  if not node_ip.nil?
+    redirect "http://#{node_ip}/SKG/config.htm"
+  else
+    redirect_url = settings.proxy_routes[device_ip]
+    redirect redirect_url if not redirect_url.nil?
+  end
 end
