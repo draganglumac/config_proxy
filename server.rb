02de1bfe928e6748919e10ede2158a1fcd4610b6
Dragan Glumac <dragan.glumac@gmail.com>
2013-12-13 16:01:22 +0000

    Changes for Sky Go Android and generalising routes.

--- server.rb
+++ server.rb
@@ -7,11 +7,11 @@ include Helpers
 
 set :dynamic_proxy_routes, {}
 
-post '/SKG/proxy/add_route' do
+post '/proxy/add_route' do
   add_route_for_source_ip_of_the_request
 end
 
-post '/SKG/proxy/remove_route' do
+post '/proxy/remove_route' do
   remove_route_for_source_ip_of_the_request
 end
 
@@ -21,14 +21,8 @@ end
 
 get '/SGA/config.json' do
   device_ip = request.env['REMOTE_ADDR']
-  node_ip = find_node_ip_from_connected_device_ip(device_ip)
-
-  if node_ip
-    redirect get_url_for_SGA_config_hosted_on_node(node_ip)
-  else
-    redirect_url = get_dynamic_redirect_url_for_device_ip(device_ip)
-    redirect redirect_url if redirect_url
-  end
+  redirect_url = get_dynamic_redirect_url_for_device_ip(device_ip)
+  redirect redirect_url if redirect_url
 end
 
 get '/SKG/config.htm' do
